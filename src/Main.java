1e31d9dbe7c2ecb3d107e4b3e66a6a689609c66c
BTHS <sample@bths.edu>
2026-01-30 10:35:19 -0500

    Employee

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        Employee employee1 = new Employee("Jim","Mason");
+        Employee employee1 = new Employee("Jim", "Mason");
         System.out.println(employee1.employeeInfo());
 
+        System.out.println(Employee.getMostRecentEmployeeID());
+        System.out.println(Employee.getTotalEmployeesCreated());
 
+        Employee employee2 = new Employee("Amy", "Adams");
+        System.out.println(employee2.employeeInfo());
+
+        System.out.println(Employee.getMostRecentEmployeeID());
+        System.out.println(Employee.getTotalEmployeesCreated());
+
+        System.out.println(employee1.employeeInfo());
+        System.out.println(employee2.employeeInfo());
+
+        Employee employee3 = new Employee("Bob", "Ross");
+        System.out.println(employee3.employeeInfo());
+
+        Employee employee4 = new Employee("Cindy", "Nox");
+        System.out.println(employee4.employeeInfo());
+
+        System.out.println(employee1.employeeInfo());
+        System.out.println(employee2.employeeInfo());
+        System.out.println(employee3.employeeInfo());
+        System.out.println(employee4.employeeInfo());
+
+        System.out.println(Employee.getMostRecentEmployeeID());
+        System.out.println(Employee.getTotalEmployeesCreated());
     }
 }
